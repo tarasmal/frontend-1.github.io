51483be988beb6f7e754d770d12397ee56b084c0
tarasmaliarchuk <tarasmalarcuk8@gmail.com>
2023-11-22 19:37:06 +0200

    task 2.2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let flag1 = true;
+let flag2 = true;
 const task1 = () => {
     const element = document.getElementById('student');
     flag1 ? element.classList.add('primary-1') : element.classList.remove('primary-1');
     flag1 = !flag1;
+}
+
+const task2 = () => {
+    const element = document.querySelector('#general-info')
+    flag2 ? element.classList.add('primary-2') : element.classList.remove('primary-2');
+    flag2 = !flag2;
 }
 
